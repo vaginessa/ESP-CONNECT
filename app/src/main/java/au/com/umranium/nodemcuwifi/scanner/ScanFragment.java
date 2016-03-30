b7be6ee07696262891674505dcbc5c92aefea051
Umran Abdulla <umran.abdulla@outware.com.au>
2016-03-30 19:35:12 +1100

    More refactoring

--- app/src/main/java/au/com/umranium/nodemcuwifi/scanner/ScanFragment.java
+++ app/src/main/java/au/com/umranium/nodemcuwifi/scanner/ScanFragment.java
@@ -88,7 +88,7 @@ public class ScanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
 
         mScanningLayout = (ViewGroup) view.findViewById(R.id.layout_scanning);
         mNoResultsLayout = (ViewGroup) view.findViewById(R.id.layout_no_results);
