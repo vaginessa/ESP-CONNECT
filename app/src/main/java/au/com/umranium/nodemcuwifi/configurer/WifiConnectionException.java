98f7c83cfc859dbac3e38dd9c72a628ff6e4cc43
Umran Abdulla <umran.abdulla@outware.com.au>
2016-04-03 11:18:38 +1000

    Add wifi connection exception class

---
+++ app/src/main/java/au/com/umranium/nodemcuwifi/configurer/WifiConnectionException.java
@@ -0,0 +1,24 @@
+package au.com.umranium.nodemcuwifi.configurer;
+
+import android.support.annotation.StringRes;
+
+/**
+ * Thrown when an error occurs while attempting to establish a wifi connection.
+ *
+ * @author umran
+ */
+public class WifiConnectionException extends Exception {
+
+    @StringRes
+    private int mMessageId;
+
+    public WifiConnectionException(@StringRes int messageId) {
+        mMessageId = messageId;
+    }
+
+    @StringRes
+    public int getMessageId() {
+        return mMessageId;
+    }
+
+}
