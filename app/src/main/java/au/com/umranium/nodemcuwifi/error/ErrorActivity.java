a440ef1e1f90c4c7e8a5cb7273830821c0f0363f
Umran Abdulla <umran.abdulla@outware.com.au>
2016-05-12 19:17:52 +1000

    Added a generic activity for displaying errors

---
+++ app/src/main/java/au/com/umranium/nodemcuwifi/error/ErrorActivity.java
@@ -0,0 +1,48 @@
+package au.com.umranium.nodemcuwifi.error;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import au.com.umranium.nodemcuwifi.R;
+import au.com.umranium.nodemcuwifi.utils.IntentExtras;
+
+/**
+ * An activity that displays a generic error.
+ */
+public class ErrorActivity extends AppCompatActivity {
+
+  private static final String PARAM_TITLE = "title";
+  private static final String PARAM_DESCRIPTION = "description";
+
+  @NonNull
+  public static Intent createIntent(@NonNull Context context,
+                                    @StringRes int title,
+                                    @StringRes int description) {
+    Intent intent = new Intent(context, ErrorActivity.class);
+    intent.putExtra(PARAM_TITLE, title);
+    intent.putExtra(PARAM_DESCRIPTION, description);
+    return intent;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_error);
+
+    int title = IntentExtras.getIntExtra(this, PARAM_TITLE);
+    int description = IntentExtras.getIntExtra(this, PARAM_DESCRIPTION);
+
+    TextView txtTitle = (TextView) findViewById(R.id.txt_title);
+    TextView txtDescription = (TextView) findViewById(R.id.txt_description);
+
+    assert txtTitle != null;
+    assert txtDescription != null;
+
+    txtTitle.setText(title);
+    txtDescription.setText(description);
+  }
+}
---
+++ app/src/main/java/au/com/umranium/nodemcuwifi/utils/IntentExtras.java
@@ -0,0 +1,30 @@
+package au.com.umranium.nodemcuwifi.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * A collection of helper functions for handling intent extras.
+ *
+ * @author umran
+ */
+public final class IntentExtras {
+
+  /**
+   * Gets and returns the intent extra identified by the key given from the activity given
+   * or throws an exception.
+   *
+   * @param activity Activity from which to extract the intent extra from
+   * @param key      Key of the extra to extract
+   * @return extra value from the activity's intent
+   */
+  public static int getIntExtra(Activity activity, String key) {
+    Intent intent = activity.getIntent();
+    if (!intent.hasExtra(key)) {
+      throw new RuntimeException(activity.getClass().getSimpleName() +
+          " was initiated without the parameter '" + key + "'");
+    }
+    return intent.getIntExtra(key, -1);
+  }
+
+}
