fc088227d5abc91b8f3aa643c148ba0cb15a575e
Umran Abdulla <umran.abdulla@outware.com.au>
2016-05-13 09:45:30 +1000

    Added connecting task activity

---
+++ app/src/main/java/au/com/umranium/nodemcuwifi/presentation/task/connecting/ConnectingActivity.java
@@ -0,0 +1,26 @@
+package au.com.umranium.nodemcuwifi.presentation.task.connecting;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import au.com.umranium.nodemcuwifi.R;
+import au.com.umranium.nodemcuwifi.presentation.task.BaseTaskActivity;
+
+/**
+ * An activity that connects to a ESP8266 node.
+ */
+public class ConnectingActivity extends BaseTaskActivity {
+
+  @NonNull
+  public static Intent createIntent(@NonNull Context context) {
+    return new Intent(context, ConnectingActivity.class);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    initUi(R.string.connecting_title, R.string.connecting_description);
+  }
+
+}
